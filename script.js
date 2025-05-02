562b8c9a8baae782f3ac0a3058d9f7ca02034136
andyc780 <coolspeed111@gmail.com>
2025-05-01 18:21:24 -0700

    Initial
    Initial Commit, added some stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('mousemove', function (cursor) {
+    // getting the circle div element
+    const circle = document.getElementById('cursor-circle');
+    // getting positions of where the mouse is, split into x and y coordinates, no clue js does this but it's cool!!
+    const positionX = cursor.clientX;
+    const positionY = cursor.clientY;
+    const positionScrollY = window.scrollY;
+    updatePosition(positionX, positionY, positionScrollY)
+
+    
+    // using left and right positioning because of the position absolute from the circle ID from the css and now we got cool mouse effects
+   function updatePosition(positionX, positionY, positionScrollY){
+        circle.style.left = `${positionX}px`;
+        circle.style.top = `${positionY + positionScrollY}px`;
+    }
+
+});
