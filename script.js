ebed713f471041facf5c264c5a5538d6dedd78c8
andyc780 <coolspeed111@gmail.com>
2025-05-02 18:21:37 -0700

    5/2/25
    added front page details

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// executes the following code when the DOM is fully loaded to prevent any 
+document.addEventListener("DOMContentLoaded", function() {
+    generateStars();
+    console.log("DOM Loaded Successfully.");
+    console.log("Window Height: " + window.innerHeight);
+    console.log("Window Width: " + window.innerWidth);
+    // for(let i = 0; i < 1000; i++){
+    //     let rand = Math.round(Math.random() * 1) + 1; // random number between 1 and 2
+    //     console.log(rand);
+    // }
+});
+
 document.addEventListener('mousemove', function (cursor) {
     // getting the circle div element
     const circle = document.getElementById('cursor-circle');
@@ -15,3 +27,33 @@ document.addEventListener('mousemove', function (cursor) {
     }
 
 });
+
+let count = 0;
+let maxStars = 80;
+function generateStars(){
+    const getDocumentBody = document.querySelector('body');
+
+    const starLoop = setInterval(() => {
+        const createStar = document.createElement('div');
+        createStar.classList.add('star-specks');
+        // getting random x and y coordinates for the stars, using the window height and width.
+        const randomX = Math.floor(Math.random() * window.innerWidth);
+        const randomY = Math.floor(Math.random() * window.innerHeight);
+        // positioning the stars aftering gathering the information.
+        createStar.style.left = `${randomX}px`;
+        createStar.style.top = `${randomY}px`;
+        // fade in animation
+        createStar.style.transition = 'opacity 0.25s ease-in';
+        setTimeout(() => {
+            createStar.style.opacity = '0.2';
+        }, 500);
+        // add the star to the document body.
+        getDocumentBody.appendChild(createStar);
+
+        count++;
+        if(count > maxStars){
+            clearInterval(starLoop);    
+        }
+    }, 20);
+    
+}
